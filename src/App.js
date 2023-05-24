3f13e92699dd5c45d83a583b105ea13aea933679
Aditya Ahuja <88674120+adityaahuja7@users.noreply.github.com>
2023-05-24 14:53:05 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Navbar from "./Components/Navbar.js";
+import "./Components/Navbar.css";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
